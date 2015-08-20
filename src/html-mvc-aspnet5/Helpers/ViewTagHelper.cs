851282b0bb5b3f501d7bb3ae2ae21c69a70c9db6
Derek Gray <dgray90@live.com>
2015-08-20 10:28:25 -0500

    Allow model name to be defined apart from model data on a view; allow model persistence to be specified on the view for creation of the script tag

--- src/html-mvc-aspnet5/Helpers/ViewTagHelper.cs
+++ src/html-mvc-aspnet5/Helpers/ViewTagHelper.cs
@@ -4,7 +4,8 @@ using System.Threading.Tasks;
 
 namespace html_mvc_aspnet5.Helpers
 {
-    [TargetElement("view", Attributes = "model")]
+    [TargetElement("view", Attributes = "model-name, model-data")]
+    [TargetElement("view", Attributes = "model-persistent")]
     [TargetElement("view", Attributes = "scope")]
     [TargetElement("view", Attributes = "name")]
     public class ViewTagHelper : TagHelper
@@ -15,32 +16,38 @@ namespace html_mvc_aspnet5.Helpers
             modelContext = context;
         }
 
-        private const string MODEL_SCOPE = "model-scope";
-
         private IJsonHelper jsonHelper;
 
         private HtmlMvcTagHelperContext modelContext;
 
         public string Name { get; set; }
-
-        public ModelExpression Model { get; set; }
+        
+        public string ModelName { get; set; }
 
         public string Scope { get; set; }
+        
+        public object ModelData { get; set; }
+
+        public bool ModelPersistent { get; set; }
 
         public override int Order { get; } = 1;
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            if (!string.IsNullOrWhiteSpace(Model?.Name) && Model.Model != null)
+            if (!string.IsNullOrWhiteSpace(ModelName) && ModelData != null)
             {
-                modelContext.Initialize(Model.Model);
+                modelContext.Initialize(ModelData);
 
                 var script = new TagBuilder("script");
-                script.Attributes.Add("model", Model.Name);
+                script.Attributes.Add("model", ModelName);
                 script.Attributes.Add("type", "application/json");
-                script.InnerHtml = jsonHelper.Serialize(Model.Model).ToString();
+                if (ModelPersistent)
+                {
+                    script.Attributes.Add("persistent", "");
+                }
+                script.InnerHtml = jsonHelper.Serialize(ModelData).ToString();
 
-                output.Attributes.Add("model", Model.Name);
+                output.Attributes.Add("model", ModelName);
                 output.PreContent.SetContent(script.ToHtmlString(TagRenderMode.Normal).ToString());
             }
             else if (!string.IsNullOrWhiteSpace(Scope))
